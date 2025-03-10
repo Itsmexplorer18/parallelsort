f8afac1ea2d8cbfb647993a693e88077628c6400
Itsmexplorer18 <124561481+Itsmexplorer18@users.noreply.github.com>
2025-03-10 18:33:24 +0530

    parallelMergeSort.hpp

--- parallelMergeSort.hpp
+++ parallelMergeSort.hpp
@@ -8,10 +8,10 @@
 
 class ParallelMergeSort {
 private:
-    std::vector<int> *nums;
+    std::vector<int> &nums;
 
 public:
-    ParallelMergeSort(std::vector<int> *nums);
+    ParallelMergeSort(std::vector<int> &nums);
     ~ParallelMergeSort();
     void sort();
     void recursiveSort(int left, int right);
