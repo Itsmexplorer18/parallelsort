a74cb996d8382e8425df2808b87f08cfce14024f
Itsmexplorer18 <124561481+Itsmexplorer18@users.noreply.github.com>
2025-03-10 18:32:29 +0530

    parallelMergeSort.cpp

--- parallelMergeSort.cpp
+++ parallelMergeSort.cpp
@@ -1,24 +1,27 @@
 #include "parallelMergeSort.hpp"
 #include <algorithm>
 
-ParallelMergeSort::ParallelMergeSort(std::vector<int> *nums) {
-    this->nums=nums;
-}
+// Scope resolution operator: here we are defining our function.
+// In the header file, only the function declarations exist.
+ParallelMergeSort::ParallelMergeSort(std::vector<int>& nums) : nums(nums) {} // Constructor initializes reference
 
+// Destructor
 ParallelMergeSort::~ParallelMergeSort() {}
 
+// Defining the recursive function here using the scope resolution operator
 void ParallelMergeSort::recursiveSort(int left, int right) {
 
     const int THRESHOLD = 5000;
 
     if (right - left < THRESHOLD) {
-        std::sort(nums->begin() + left, nums->begin() + right + 1);
+        std::sort(nums.begin() + left, nums.begin() + right + 1);
         return;
     }
-    
+
     if (left >= right) {
         return;
     }
+
     int mid = left + (right - left) / 2;
 
     std::thread thread_1([this, left, mid] { this->recursiveSort(left, mid); });
@@ -31,35 +34,36 @@ void ParallelMergeSort::recursiveSort(int left, int right) {
     int j = mid + 1;
 
     while (i <= mid && j <= right) {
-        if ((*nums)[i] <= (*nums)[j]) {
-            result.push_back((*nums)[i]);
+        if (nums[i] <= nums[j]) {
+            result.push_back(nums[i]);
             i++;
         } else {
-            result.push_back((*nums)[j]);
+            result.push_back(nums[j]);
             j++;
         }
     }
 
     while (i <= mid) {
-        result.push_back((*nums)[i]);
+        result.push_back(nums[i]);
         i++;
     }
 
     while (j <= right) {
-        result.push_back((*nums)[j]);
+        result.push_back(nums[j]);
         j++;
     }
 
     for (int k = 0; k < result.size(); k++) {
-        (*nums)[left + k] = result[k];
+        nums[left + k] = result[k];
     }
 }
 
+// Sort function that starts the parallel recursive sort
 void ParallelMergeSort::sort() {
-    if ((*nums).size() == 0) {
-        exit(1);
+    if (nums.empty()) { // Check for an empty vector
+        return;
     }
 
-    std::thread thread_1([this] { this->recursiveSort(0, (*nums).size() - 1); });
+    std::thread thread_1([this] { this->recursiveSort(0, nums.size() - 1); });
     thread_1.join();
 }
